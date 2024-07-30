6f35b8c485a7ca1bceab5720c7e3595869f0675d
EternalRival <eternalrival8@gmail.com>
2024-07-30 04:53:30 +0300

    feat: add text labels to slices

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,21 @@
 import { TableRow } from '~/features/wheel/components/model/table-row.type';
 import { Wheel } from '~/features/wheel/components/wheel';
+import { getRandomNumber } from '~/utils/get-random-number';
 
 const tableMock: TableRow[] = [
-  { label: '', score: 200 },
-  { label: '', score: 400 },
-  { label: '', score: 800 },
-  { label: '', score: 1600 },
-  { label: '', score: 3200 },
-  { label: '', score: 6400 },
-  { label: '', score: 12800 },
-  { label: '', score: 25600 },
-];
+  { label: 'firth', score: 2 ** 1 },
+  { label: 'second', score: 2 ** 2 },
+  { label: 'third', score: 2 ** 3 },
+  { label: 'fourth', score: 2 ** 4 },
+  { label: 'fifth', score: 2 ** 5 },
+  { label: 'sixth', score: 2 ** 6 },
+  { label: 'seventh', score: 2 ** 7 },
+  { label: 'eighth', score: 2 ** 8 },
+].sort(() => getRandomNumber(-1, 1));
 
 export class App {
   render(root: HTMLElement) {
-    const wheel = new Wheel({ size: 512, padding: 16, table: tableMock });
+    const wheel = new Wheel({ size: 512, table: tableMock });
 
     root.append(wheel.getNode());
   }
--- src/features/wheel/components/wheel.ts
+++ src/features/wheel/components/wheel.ts
@@ -3,10 +3,12 @@ import { getSliceList } from './lib/get-slice-list';
 import { TableRow } from './model/table-row.type';
 import { WheelSlice } from './model/wheel-slice.type';
 
+const CIRCLE = Math.PI * 2;
+
 export class Wheel extends Component {
   private ctx: CanvasRenderingContext2D;
 
-  constructor({ size = 512, padding = 0, table }: { size?: number; padding?: number; table: TableRow[] }) {
+  constructor({ size = 512, table }: { size?: number; table: TableRow[] }) {
     super('div');
 
     const canvas = new Component('canvas', { width: size, height: size, textContent: 'wheel of fortune' });
@@ -16,54 +18,19 @@ export class Wheel extends Component {
 
     this.ctx = ctx;
 
-    const radius = size / 2;
     const sliceList = getSliceList(table);
-    this.drawWheel({ padding, radius, sliceList });
+    this.drawWheel({ size, sliceList });
 
     this.append(canvas);
   }
 
-  private drawSlice({ radius, padding, slice }: { radius: number; padding: number; slice: WheelSlice }) {
-    const { ctx } = this;
-    const { startAngle, endAngle, color } = slice;
-
-    ctx.save();
-
-    const slicePath = new Path2D();
-    slicePath.moveTo(radius, radius);
-    slicePath.arc(radius, radius, radius - padding, startAngle, endAngle);
-    slicePath.lineTo(radius, radius);
-
-    ctx.fillStyle = color;
-
-    ctx.fill(slicePath);
-    ctx.stroke(slicePath);
-
-    ctx.restore();
-  }
-
-  private drawCenter({ radius, color }: { radius: number; color: string }) {
-    const { ctx } = this;
-
-    ctx.save();
-
-    const centerPath = new Path2D();
-    centerPath.arc(radius, radius, 30, 0, 2 * Math.PI);
-
-    ctx.fillStyle = color;
-    ctx.fill(centerPath);
-    ctx.stroke(centerPath);
-
-    ctx.restore();
-  }
-
-  private drawShadow({ radius, padding }: { radius: number; padding: number }) {
+  private drawShadow({ center, wheelRadius, padding }: { center: number; wheelRadius: number; padding: number }): void {
     const { ctx } = this;
 
     ctx.save();
 
     const shadowPath = new Path2D();
-    shadowPath.arc(radius, radius, radius - padding, 0, Math.PI * 2);
+    shadowPath.arc(center, center, wheelRadius, 0, CIRCLE);
     ctx.shadowColor = '#000';
     ctx.shadowBlur = padding;
     ctx.fill(shadowPath);
@@ -72,11 +39,101 @@ export class Wheel extends Component {
     ctx.restore();
   }
 
-  private drawWheel({ radius, padding, sliceList }: { radius: number; padding: number; sliceList: WheelSlice[] }) {
-    this.drawShadow({ radius, padding });
+  private drawSlice({
+    center,
+    wheelRadius,
+    centerCircleRadius,
+    fontSize,
+    slice,
+  }: {
+    center: number;
+    wheelRadius: number;
+    centerCircleRadius: number;
+    fontSize: number;
+    slice: WheelSlice;
+  }): void {
+    const { ctx } = this;
+    const { startAngle, endAngle, color, label } = slice;
 
-    sliceList.forEach((slice) => this.drawSlice({ radius, padding, slice }));
+    ctx.save();
 
-    this.drawCenter({ radius, color: 'teal' });
+    const slicePath = new Path2D();
+    slicePath.moveTo(center, center);
+    slicePath.arc(center, center, wheelRadius, startAngle, endAngle);
+    slicePath.lineTo(center, center);
+
+    ctx.fillStyle = color;
+
+    ctx.fill(slicePath);
+    ctx.stroke(slicePath);
+
+    if (endAngle - startAngle > 0.25) {
+      ctx.translate(center, center);
+      ctx.rotate(endAngle - (endAngle - startAngle) / 2);
+
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.font = `${fontSize}px sans-serif`;
+
+      ctx.lineWidth = fontSize / 8;
+      ctx.strokeStyle = '#000';
+      ctx.fillStyle = '#FFF';
+      ctx.shadowColor = '#00000080';
+      ctx.shadowBlur = fontSize / 2;
+
+      const text = this.normalizeText(label, wheelRadius - centerCircleRadius * 2);
+
+      ctx.strokeText(text, (wheelRadius + centerCircleRadius) / 2, 0);
+      ctx.fillText(text, (wheelRadius + centerCircleRadius) / 2, 0);
+    }
+
+    ctx.restore();
+  }
+
+  private normalizeText(text: string, maxWidth: number): string {
+    const { actualBoundingBoxLeft, actualBoundingBoxRight } = this.ctx.measureText(text);
+
+    if (actualBoundingBoxRight + actualBoundingBoxLeft < maxWidth) {
+      return text;
+    }
+
+    return this.normalizeText(`${text.slice(0, -2)}…`, maxWidth);
+  }
+
+  private drawCenterCircle({
+    center,
+    centerCircleRadius,
+    color,
+  }: {
+    center: number;
+    centerCircleRadius: number;
+    color: string;
+  }): void {
+    const { ctx } = this;
+
+    ctx.save();
+
+    const centerPath = new Path2D();
+    centerPath.arc(center, center, centerCircleRadius, 0, CIRCLE);
+
+    ctx.fillStyle = color;
+    ctx.fill(centerPath);
+    ctx.stroke(centerPath);
+
+    ctx.restore();
+  }
+
+  private drawWheel({ size, sliceList }: { size: number; sliceList: WheelSlice[] }): void {
+    const center = size / 2;
+    const padding = size / 32;
+    const fontSize = size / 32;
+    const wheelRadius = center - padding;
+    const centerCircleRadius = wheelRadius / 8;
+
+    this.drawShadow({ center, wheelRadius, padding });
+
+    sliceList.forEach((slice) => this.drawSlice({ center, wheelRadius, centerCircleRadius, fontSize, slice }));
+
+    this.drawCenterCircle({ center, centerCircleRadius, color: 'teal' });
   }
 }
