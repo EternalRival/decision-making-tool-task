2027d6d14a09211b5bd6ca3206fc2c73226c6cff
EternalRival <eternalrival8@gmail.com>
2024-08-09 01:48:25 +0300

    build: add custom script to generate tor page

---
+++ build-tor-page.mjs
@@ -0,0 +1,36 @@
+import mds from 'markdown-styles';
+import { copyFileSync, mkdirSync, rmSync } from 'node:fs';
+import { resolve } from 'node:path';
+
+const sourceFilePath = resolve('README.md');
+const tempDirPath = resolve('dist/tor-temp');
+const tempFilePath = resolve(tempDirPath, 'index.md');
+const torPageDirPath = resolve('dist/tor');
+
+const consoleMute = {
+  log: console.log,
+  enable() {
+    console.log = () => {};
+  },
+  disable() {
+    console.log = this.log;
+  },
+};
+
+mkdirSync(tempDirPath, { recursive: true });
+copyFileSync(sourceFilePath, tempFilePath);
+
+consoleMute.enable();
+
+mds.render(
+  mds.resolveArgs({
+    input: resolve(tempDirPath),
+    output: resolve(torPageDirPath),
+    layout: 'mixu-bootstrap-2col',
+  }),
+  () => {
+    rmSync(tempDirPath, { recursive: true, force: true });
+    consoleMute.disable();
+    console.log('ToR page builded!');
+  }
+);
