c0f4ff04ecbc2db517cc64a6142cf499a5da9987
EternalRival <eternalrival8@gmail.com>
2024-08-17 15:08:08 +0300

    fix: update line-height of inputs

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -2,6 +2,9 @@ import type { Config } from 'stylelint';
 
 const config: Config = {
   extends: ['stylelint-config-standard', 'stylelint-config-clean-order'],
+  rules: {
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
+  },
 };
 
 export default config;
