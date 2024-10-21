c515a198da4d443b2095a90f3b875bd697135f13
EternalRival <eternalrival8@gmail.com>
2024-10-21 18:29:49 +0300

    refactor: move assets to public

--- src/widgets/wheel/utils/get-sound-icon.ts
+++ src/widgets/wheel/utils/get-sound-icon.ts
@@ -1,5 +1,5 @@
-import soundIconSrc from '../assets/sound-icon.svg?url';
+const ICON_URL = '/assets/sound-icon.svg';
 
 export default function getSoundIcon({ name }: { name: 'on' | 'off' }): string {
-  return `<svg viewBox="0 0 24 24" width="24" height="24"><use href=${soundIconSrc}#${name} /></svg>`;
+  return `<svg viewBox="0 0 24 24" width="24" height="24"><use href=${ICON_URL}#${name} /></svg>`;
 }
--- src/widgets/wheel/utils/play-winning-sound.ts
+++ src/widgets/wheel/utils/play-winning-sound.ts
@@ -1,6 +1,4 @@
-import winningSoundSrc from '../assets/ta-da.mp3';
-
-const audio = Object.assign(new Audio(winningSoundSrc), { volume: 0.5 });
+const audio = Object.assign(new Audio('/assets/ta-da.mp3'), { volume: 0.5 });
 
 export default function playWinningSound(): Promise<void> {
   audio.currentTime = 0;
