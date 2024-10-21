572f1a496391d13c047484bdb8ce72f1fd0012fb
EternalRival <eternalrival8@gmail.com>
2024-10-21 18:22:18 +0300

    fix: update svg sprite url

--- src/widgets/wheel/utils/get-sound-icon.ts
+++ src/widgets/wheel/utils/get-sound-icon.ts
@@ -1,4 +1,4 @@
-import soundIconSrc from '../assets/sound-icon.svg';
+import soundIconSrc from '../assets/sound-icon.svg?url';
 
 export default function getSoundIcon({ name }: { name: 'on' | 'off' }): string {
   return `<svg viewBox="0 0 24 24" width="24" height="24"><use href=${soundIconSrc}#${name} /></svg>`;
